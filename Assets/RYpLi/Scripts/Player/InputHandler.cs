64b17e0926ef9d0f5f461b8445e77c4b4df40803
RYpLi <zjanasek27@gmail.com>
2025-04-08 22:14:56 +0200

    JUMP!!

--- Assets/RYpLi/Scripts/Player/InputHandler.cs
+++ Assets/RYpLi/Scripts/Player/InputHandler.cs
@@ -5,12 +5,15 @@ public class InputHandler : MonoBehaviour
 {
     public PlayerController CharacterController;
 
-    private InputAction _moveAction, _lookAction;
+    private InputAction _moveAction, _lookAction, _jumpAction;
 
     void Start ()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
         _lookAction = InputSystem.actions.FindAction("Look");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+
+        _jumpAction.performed += OnJumpPerformed;
 
         Cursor.visible = false;
     }
@@ -22,6 +25,10 @@ public class InputHandler : MonoBehaviour
 
         Vector2 lookVector = _lookAction.ReadValue<Vector2>();
         CharacterController.Rotate(lookVector);
+    }
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        CharacterController.Jump();
     }
 
 
