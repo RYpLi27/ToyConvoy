34e41275e5ba6a447230eb6e3fbc2e717cd3a0c0
ZinneX <robertmazurkiewicz1112@gmail.com>
2025-04-30 17:51:51 +0200

    Baza Gameloop
    Mozna gre przejsc
    Nie da sie jeszcze przegrac
    Da sie pauzowac i wychodzic do menu i z gry

--- Assets/ZinneX/Scripts/Other/UIButtons.cs
+++ Assets/ZinneX/Scripts/Other/UIButtons.cs
@@ -10,4 +10,9 @@ public class UIButtons : MonoBehaviour
     public void ExitGame() {
         Application.Quit();
     }
+
+    public void ResetScaleAndGameState() {
+        Time.timeScale = 1;
+        if (GameManager.instance != null) { GameManager.gameState = GameManager.GameState.Ongoing;}
+    }
 }
