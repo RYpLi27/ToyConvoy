d7256090e31386c6ace7dde22e8354aa97820f16
ZinneX <robertmazurkiewicz1112@gmail.com>
2025-05-07 23:29:28 +0200

    Paski na shaderach i fix trapow
    Trapy juz sa o wiele bardziej intuicyjne bo sprawdzaja wszystkie rogi + srodek czy stoja na drodze (dalej lekko wystaje ale ma calkiem niezly feeling dlatego zostawilem)
    
    Paski zdrowia na shaderach + dotweens juz sa zrobione ale dalej nie sa zoptymalizowane bo na to mi zabraklo czasu dzisiaj i ogarne to jutro / w nastepnym tygodniu

--- Assets/ZinneX/Scripts/Player/StaminaManager.cs
+++ Assets/ZinneX/Scripts/Player/StaminaManager.cs
@@ -7,13 +7,15 @@ public class StaminaManager : MonoBehaviour
     [SerializeField] private float stamina;
     [SerializeField] private float staminaRecoverAmount;
     [SerializeField] private float staminaRecoveryTime;
-    [SerializeField] private Slider staminaBar;
+    [SerializeField] private Bar staminaBar;
     [SerializeField] private Image fillArea;
-    [SerializeField] private Color exhaustedBarColor;
-    [SerializeField] private Color normalBarColor;
+    [SerializeField] private Material exhaustedBarColor;
+    [SerializeField] private Material normalBarColor;
     private float currentStamina, lastStaminaUse;
     [ReadOnly] public bool isExhausted;
 
+    private bool trailCatchUp;
+    
     private void Start() {
         currentStamina = stamina;
     }
@@ -26,9 +28,13 @@ public class StaminaManager : MonoBehaviour
     }
     
     private void HandleStamina() {
-        if (Time.time - lastStaminaUse > staminaRecoveryTime) {
+        if (Time.time - lastStaminaUse > staminaRecoveryTime / 4 && trailCatchUp) {
+            UpdateUI(false);
+            trailCatchUp = false;
+        } else if (Time.time - lastStaminaUse > staminaRecoveryTime) {
             currentStamina = Mathf.Min(currentStamina + Time.deltaTime * staminaRecoverAmount, stamina);
-            UpdateUI();
+            
+            UpdateUI(false, true);
         }
 
         if (currentStamina == 0) isExhausted = true;
@@ -39,13 +45,27 @@ public class StaminaManager : MonoBehaviour
     public void DrainStamina(float staminaCost) {
         currentStamina = Mathf.Max(currentStamina - staminaCost, 0);
         lastStaminaUse = Time.time;
+
+        trailCatchUp = true;
         
-        UpdateUI();
+        UpdateUI(true);
     }
 
-    private void UpdateUI() {
-        fillArea.color = isExhausted == true ? exhaustedBarColor : normalBarColor;
-        
-        staminaBar.value = currentStamina;
+    private void UpdateUI(bool instant, bool withTrail = false) {
+        fillArea.material = isExhausted == true ? exhaustedBarColor : normalBarColor;
+
+        switch (instant, withTrail) {
+            case (true, false):
+                staminaBar.UpdateUIInstant(currentStamina, stamina);
+                break;
+            
+            case (false, true):
+                staminaBar.UpdateUIInstantWithTrail(currentStamina, stamina);
+                break;
+            
+            case (false, false):
+                staminaBar.UpdateUI(currentStamina, stamina);
+                break;
+        }
     }
 }
