e9a9c46d0068bbf7118ae1c4fe7b71dde7a7136e
ZinneX <robertmazurkiewicz1112@gmail.com>
2025-05-20 18:37:07 +0200

    Upgrade Turret + poprawa animacji golda
    Da sie ulepszac turret do 3 poziomu kazdy ale jeszcze nie sa dobrze zbalansowane.
    
    Popup o zdobytym goldzie sie teraz kolejkuje i wjezdza zza ekranu

--- Assets/ZinneX/Scripts/Buildings/Traps/SpikesSO.cs
+++ Assets/ZinneX/Scripts/Buildings/Traps/SpikesSO.cs
@@ -1,13 +1,19 @@
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 [System.Serializable]
 [CreateAssetMenu(fileName = "New SpikesSO", menuName = "Scriptable Objects/Traps/Spikes")]
 public class SpikesSO : BuildingSO {
-    public float damagePerSecond;
+    public List<TurretStats> turretStats;
     
     [Title("Preferably between 12-36")] 
     public float hitInstancesPerSecond;
-    
     public Material objectMaterial;
+
+    [System.Serializable]
+    public class TurretStats {
+        public float damagePerSecond;
+        public int upgradePrice;
+    }
 }
--- Assets/ZinneX/Scripts/Buildings/Turrets/NonHomingTurret.cs
+++ Assets/ZinneX/Scripts/Buildings/Turrets/NonHomingTurret.cs
@@ -8,9 +8,9 @@ public class NonHomingTurret : TurretSO {
     public AnimationCurve heightCurve;
     public float movementPredictDistance;
 
-    public override void Shoot(Transform firepoint, Transform target) {
+    public override void Shoot(Transform firepoint, Transform target, TurretStats stats) {
         GameObject projectile = ObjectPoolManager.SpawnObject(projectilePrefab, firepoint.position, Quaternion.identity, ObjectPoolManager.PoolingParent.Projectile);
-        projectile.GetComponent<NonHomingProjectile>().SetupProjectile(this, target.position, target.GetComponent<EnemyBehaviour>().MoveDir);
+        projectile.GetComponent<NonHomingProjectile>().SetupProjectile(this, target.position, target.GetComponent<EnemyBehaviour>().MoveDir, stats);
         // BULLETS LOGIC LIKE TRAJECTORY AND LAUNCHING AT TARGET IS ALL MADE IN PROJECTILE SCRIPT
     }
 }
