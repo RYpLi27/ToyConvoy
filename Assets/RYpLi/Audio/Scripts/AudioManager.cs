f2ca24641f7989aba815a871374c71778b7b1fef
Zbigniew "RYpLi" Janasek <zjanasek@edu.cdv.pl>
2025-06-08 15:45:21 +0200

    SHOTS FIRED

---
+++ Assets/RYpLi/Audio/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance
+    {
+        get; private set;
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than 1 audio manager on the scene");
+        }
+        instance = this;
+    }
+    public void playOneShot(EventReference sound, Vector3 wolrdPos)
+    {
+        RuntimeManager.PlayOneShot(sound, wolrdPos);
+    }
+}
--- Assets/ZinneX/Scripts/Weapons/Weapon.cs
+++ Assets/ZinneX/Scripts/Weapons/Weapon.cs
@@ -2,11 +2,13 @@ using DG.Tweening;
 using Sirenix.OdinInspector;
 using Unity.Cinemachine;
 using UnityEngine;
+using FMODUnity;
 
 public class Weapon : MonoBehaviour {
     [SerializeField] private WeaponSO weaponSO;
     [SerializeField] private Transform firePoint;
     [SerializeField] private LayerMask whatIsTarget;
+    [SerializeField] private EventReference shotFired;
     // [SerializeField] private Material enemyHitMaterial;
     // [SerializeField] private Material obstacleHitMaterial;
     [SerializeField] private GameObject damageText;
@@ -50,6 +52,8 @@ public class Weapon : MonoBehaviour {
             }
 
             CreateMuzzleFlash();
+            AudioManager.instance.playOneShot(shotFired, this.transform.position);
+            Debug.Log("audio");
         }
 
         WeaponRecoil.instance.ApplyRecoil(weaponSO.cameraVerticalRecoil, weaponSO.cameraHorizontalRecoil);
