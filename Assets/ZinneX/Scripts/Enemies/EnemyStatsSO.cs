428785be2febb65ba504deeb41ec9473c2c5212e
ZinneX <robertmazurkiewicz1112@gmail.com>
2025-06-29 19:55:46 +0200

    Gotowe staty + pare modeli podmienionych
    Staty są gotowe dla każdego przeciwnika, ale nie ma jeszcze prefabów bo nie ma modeli.
    
    Podmieniłem modele do paru turretów.

--- Assets/ZinneX/Scripts/Enemies/EnemyStatsSO.cs
+++ Assets/ZinneX/Scripts/Enemies/EnemyStatsSO.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemyStatsSO", menuName = "Scriptable Objects/Stats/Enemy Stats")]
@@ -5,4 +6,6 @@ public class EnemyStatsSO : StatsSO
 {
     public float moveSpeed;
     public int damageToBase;
+    [SerializeField] [HorizontalGroup("Gold")] public int minGold;
+    [SerializeField] [HorizontalGroup("Gold")] public int maxGold;
 }
